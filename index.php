8da81ef38e9c46bd2afceb1e200df29c28715021
Antonio69999 <antoine.chabeauti@gmail.com>
2023-06-28 09:44:06 +0200

    php_part4

---
+++ index.php
@@ -0,0 +1,124 @@
+<?php
+
+//Exercice_1
+
+// $isTrue = 1;
+
+// function returnTrue($isTrue)
+// {
+//     return true;
+// }
+
+// echo returnTrue($isTrue);
+
+
+//Exercice_2
+
+// $isString = "henlo";
+
+// function string($isString)
+// {
+//     return $isString;
+// }
+
+// echo string($isString);
+
+//Exercice_3
+
+// $isString1 = "henlo";
+// $isString2 = "howrU";
+
+// function doubleString($isString1, $isString2)
+// {
+//     return ($isString1 . $isString2);
+// }
+
+// echo doubleString($isString1, $isString2);
+
+//Exercice 4
+
+// $num1 = 10;
+// $num2 = 10;
+
+// function testNum ($num1, $num2)
+// {
+//     if ($num1 > $num2) {
+//         return ("Le premier nombre est plus grand");
+//     }
+
+//     elseif ($num1 < $num2) {
+//         return ("Le premier nombre est plus petit");
+//     }
+
+//     elseif ($num1 == $num2) {
+//         return ("Les deux nombres sont identiques");
+//     }
+// }
+
+// echo testNum($num1, $num2);
+
+//Exercice 5
+
+// $num = 10;
+// $string = "henlo";
+
+// function concatener ($num, $string)
+// {
+//     return ($num . $string);
+// }
+
+// echo concatener($num, $string);
+
+//Exercice 6 
+// $lastName = "Toto";
+// $firstName = "Tata";
+// $age = "20";
+
+// function conca($lastName, $firstName, $age)
+// {
+//     return ("Bonjour"." ".$firstName." ".$lastName." ".",tu as"." ".$age."ans.");
+// }
+
+// echo conca($lastName, $firstName, $age);
+
+//Exercice 7 
+
+// $age = 22;
+// $gender = "Homme";
+
+// function genderAge ($age, $gender)
+// {
+//     if ($gender == "Homme" && $age >= 18) {
+//         return ("Vous êtes un homme et vous êtes majeur");
+//     }
+
+//     elseif ($gender == "Homme" && $age < 18) {
+//         return ("Vous êtes un homme et vous êtes mineur");
+//     }
+
+//     elseif ($gender == "Femme" && $age >= 18) {
+//         return ("Vous êtes une femme et vous êtes majeur");
+//     }
+
+//     elseif ($gender == "Femme" && $age < 18) {
+//         return ("Vous êtes une femme et vous êtes mineur");
+//     }
+// }
+
+// echo genderAge($age, $gender);
+
+//Exercice 8 
+
+// $num1=1;
+// $num2=2;
+// $num3=3;
+
+// function somme($num1, $num2, $num3) 
+// {
+//     return ($num1 + $num2 + $num3);
+
+// }
+
+// echo somme($num1, $num2, $num3);
+
+?>
